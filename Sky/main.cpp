1fa4547e7dd8f393204b31232e70cfd10583c1a4
Morteza Ahmadi <morinob93@gmail.com>
2020-01-16 21:26:05 -0500

    Initial commit

---
+++ Sky/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Sky
+//
+//  Created by Morteza Ahmadi on 2020-01-16.
+//  Copyright © 2020 Morteza Ahmadi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
