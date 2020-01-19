f696459e809c81063fa1d0f86df5974230385af2
Morteza Ahmadi <morinob93@gmail.com>
2020-01-19 13:30:23 -0500

    Add token type enum + some attributes of tokenizer class

--- Sky/Lexer/Token.hpp
+++ Sky/Lexer/Token.hpp
@@ -10,5 +10,72 @@
 #define Token_hpp
 
 #include <stdio.h>
+#include <string>
+enum class TokenType {
+    Id, // letter alphanum*
+    Alphanum, // letter | digit | _
+    Integer, // nonzero digit* | 0
+    Float, // integer fraction [e[+|-] integer]
+    Fraction, // .digit* nonzero | .0
+    Letter, // a..z | A..Z
+    Digit, // 0..9
+    NonZero, // 1..9
+    Eq, // ==
+    NotEq, // <>
+    Lt, // <
+    Gt, // >
+    LEq, // <=
+    GEq, // >=
+    Plus, // +
+    Minus, // -
+    Mult, // *
+    Div, // /
+    Asgn, // =
+    OpenPr, // (
+    ClosePr, // )
+    OpenCbr, // {
+    CloseCbr, // }
+    OpenSqbr, // [
+    CloseSqbr, // ]
+    Semi, // ;
+    Comma, // ,
+    Dot, // .
+    Colon, // :
+    DblColon, // ::
+    InlineCmt, // //
+    BlockOpenCmt, // /*
+    BlockCloseCmt, // */
+};
 
+enum class ReservedWord {
+    If,
+    Then,
+    Else,
+    Integer,
+    Float,
+    While,
+    Class,
+    Do,
+    End,
+    Public,
+    Private,
+    Or,
+    And,
+    Not,
+    Read,
+    Write,
+    Return,
+    Main,
+    Inherits,
+    Local,
+};
+
+class Token {
+public:
+    
+    TokenType type;
+    int lineno;
+    std::string value;
+    
+};
 #endif /* Token_hpp */
--- Sky/Lexer/Tokenizer.hpp
+++ Sky/Lexer/Tokenizer.hpp
@@ -10,5 +10,23 @@
 #define Tokenizer_hpp
 
 #include <stdio.h>
+#include <string>
+#include <vector>
+#include "Token.hpp"
+
+class Tokenizer {
+public:
+    Tokenizer(std::string word);
+    ~Tokenizer();
+    std::vector<Token*> matches;
+    
+private:
+    std::string word;
+    Token isInteger(std::string);
+    Token isFloat(std::string);
+    Token isID(std::string);
+    Token isKeyword(std::string);
+    
+};
 
 #endif /* Tokenizer_hpp */
