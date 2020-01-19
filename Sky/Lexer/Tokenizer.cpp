d8176e1997dd355ecc0040afeca2d8315e6e1c02
Morteza Ahmadi <morinob93@gmail.com>
2020-01-19 18:46:28 -0500

    Add regex matching for isID function

--- Sky/Lexer/Tokenizer.cpp
+++ Sky/Lexer/Tokenizer.cpp
@@ -28,15 +28,23 @@ Token* Tokenizer::isInteger(std::string word, int lineno) {
 
 Token* Tokenizer::isFloat(std::string word, int lineno) {
     std::regex reg ("(([1-9][0-9]*|0)(.[0-9]*[1-9]|.0)([e]?[+-]?[1-9][0-9]*|0))");
-    if ( std:: regex_match(word, reg) ) {
+    if (std:: regex_match(word, reg)) {
         Token* floatToken = new Token(TokenType::Float, lineno, word);
         return floatToken;
     } else return nullptr;
 }
 
 Token* Tokenizer::isID(std::string word, int lineno) {
-    
-    return nullptr;
+    std::regex reg ("(([a-z]|[A-Z])([a-z]|[A-Z]|[1-9][0-9]*|_)*)");
+    if (std::regex_match(word, reg)) {
+        if (RESERVED_WORDS.find(word) != RESERVED_WORDS.end()) {
+            Token* reservedWordToken = new Token(RESERVED_WORDS.at(word), lineno, word);
+            return reservedWordToken;
+        } else {
+            Token* idToken = new Token(TokenType::Id, lineno, word);
+            return idToken;
+        }
+    } else return nullptr;
 }
 
 Token* Tokenizer::isKeyword(std::string word, int lineno) {
