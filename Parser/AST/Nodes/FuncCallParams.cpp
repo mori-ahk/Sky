4781be7ea0a550b1bfda7dee510aee3decb50be5
Morteza Ahmadi <morinob93@gmail.com>
2020-04-14 00:12:42 -0400

    Add code generation for function call

---
+++ Parser/AST/Nodes/FuncCallParams.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Morteza Ahmadi on 2020-04-12.
+//
+
+#include "FuncCallParams.h"
---
+++ Parser/AST/Nodes/FuncCallParams.h
@@ -0,0 +1,14 @@
+//
+// Created by Morteza Ahmadi on 2020-04-12.
+//
+
+#ifndef SKY_FUNCCALLPARAMS_H
+#define SKY_FUNCCALLPARAMS_H
+
+
+class FuncCallParams {
+
+};
+
+
+#endif //SKY_FUNCCALLPARAMS_H
